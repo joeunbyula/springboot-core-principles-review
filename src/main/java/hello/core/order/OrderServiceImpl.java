0c012330e3943a6733cb2d5863fee39f687ff59a
eun-byul-jo <eunbyul.jo@steppay.kr>
2023-10-19 13:50:17 +0900

    다양한 의존관계 주입 방법

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,17 +9,35 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
-    //인터페이스에도 의존, 구현체에도 의존하고있는 문제점이 발생!
+    //1) 인터페이스에도 의존, 구현체에도 의존하고있는 문제점이 발생!
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    private DiscountPolicy discountPolicy;
-    private final MemberRepository memberRepository;
 
-    @Autowired
+    //2) 생성자 의존성 주입
+    private final DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
+//
+//    //@Autowired //생성자가 하나인 경우엔 생략 가능!
     public OrderServiceImpl(MemberRepository memberRepository,DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+    //3)수정자 주입 (@AutoWired(required=false)를 사용하면 선택적으로 사용가능(빈에 등록이 되어있지 않아도 사용가능)
+//    private DiscountPolicy discountPolicy;
+//    private MemberRepository memberRepository;
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+    //4) 필드 주입 ( 권장하지않는 방법이다.. 외부에서 변경이 불가능하다 )
+//    @Autowired private MemberRepository memberRepository;
+//    @Autowired private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
